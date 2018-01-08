6bc630b9f35cbe8c34aecf8e7c0e661b4538ed2f
shixiangyu216 <773681606@qq.com>
2018-01-08 11:20:07 +0800

    捞钱计划

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '我老婆' });
+  res.render('index', { title: '我超可爱的 请给我打钱' });
 });
 
 module.exports = router;
